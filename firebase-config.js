420b8ca70c3bf751e6006d39de43301c87ba3c24
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-18 01:02:47 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // আপনার Firebase প্রজেক্টের কনফিগারেশন
 const firebaseConfig = {
-    apiKey: "AIzaSyAmdXx4PAETjr42lpSbTR6sb5H0WqUyzoo",
-    authDomain: "totolive-6689f.firebaseapp.com",
-    databaseURL: "https://totolive-6689f-default-rtdb.firebaseio.com",
-    projectId: "totolive-6689f",
+    apiKey: "AIzaSyBl5hsOW7e4Hqr6_hvN0Qo-WsEEQOHzUKk",
+    authDomain: "mini-nahidx07.firebaseapp.com",
+    databaseURL: "https://mini-nahidx07-default-rtdb.firebaseio.com",
+    projectId: "mini-nahidx07",
     storageBucket: "", // খালি রাখুন, Storage ব্যবহার করছি না
-    messagingSenderId: "423841122771",
-    appId: "1:423841122771:web:ad3c529d8dc9833a69c726"
+    messagingSenderId: "916637371728",
+    appId: "1:916637371728:web:11ca2ba9872245a322b055"
 };
